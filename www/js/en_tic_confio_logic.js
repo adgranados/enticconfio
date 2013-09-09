29c23684ee73118c967c09c5384f3656f945b70e
Daniel <adgranados@gmail.com>
2013-09-09 09:33:56 -0500

    Share on Twitter

--- www/js/en_tic_confio_logic.js
+++ www/js/en_tic_confio_logic.js
@@ -36,13 +36,12 @@ twitterGo = function(url,title){
 	sessionStorage.setItem("compartirurl",url);
 
 	if(title == null || title == 'null'){
-		title="Nooooooooo"
+		title=" "
 	}
 
 	if(url != null){
 		//window.open("https://twitter.com/share?url='"+url+"'&screen_name=enticconfio&text="+title, '_blank');
-		window.open('https://twitter.com/share?url='+url, '_blank');
-		//window.open('https://twitter.com/share?url='+url+'&screen_name=enticconfio&text='+title, '_blank');
+		window.open('https://twitter.com/share?url='+url+'&screen_name=enticconfio&text='+title, '_blank');
 		//window.open('https://twitter.com/share?url=http://www.enticconfio.gov.co/&screen_name=enticconfio&text=Probando..', '_blank');
 	}
 }
@@ -69,7 +68,7 @@ twitterGo = function(url,title){
 	articulo.append("<h3>"+title+"</h3>");	
 	 var vermasLinks = "";
 			if(vermas!=null && vermas != "" && vermas.match(/null/g)==null){
-				vermasLinks = '<div data-role="navbar"><ul><li><a href="#" onclick="vermas(\''+vermas+'\')" data-role="button" data-theme="b">ver mas</a></li><li>'+'<a href="#" onclick="facebookGo(\''+vermas+'\')" rel="external"  data-role="button" data-theme="b">Compartir en Facebook!!</a></li><li><a href="#" onclick="twitterGo(\''+vermas+','+title+'\')" rel="external"  data-role="button" data-theme="b">Compartir en Twitter!!</a></li></ul></div>';
+				vermasLinks = '<div data-role="navbar"><ul><li><a href="#" onclick="vermas(\''+vermas+'\')" data-role="button" data-theme="b">ver mas</a></li><li>'+'<a href="#" onclick="facebookGo(\''+vermas+'\')" rel="external"  data-role="button" data-theme="b">Compartir en Facebook!!</a></li><li><a href="#" onclick="twitterGo(\''+vermas+'\',\''+title+'\')" rel="external"  data-role="button" data-theme="b">Compartir en Twitter!!</a></li></ul></div>';
 				//href="https://www.facebook.com/sharer/sharer.php?u='+vermas+'" target="_blank"
 				//'+vermas+'\'
 				

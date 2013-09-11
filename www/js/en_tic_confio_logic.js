4c8dd3ccf3d95ff6c82a0f837cad6610eca6fdc6
Daniel <adgranados@gmail.com>
2013-09-10 21:32:12 -0500

    falta cerrar una comilla

--- www/js/en_tic_confio_logic.js
+++ www/js/en_tic_confio_logic.js
@@ -40,7 +40,7 @@ twitterGo = function(url,title){
 	}*/
 
 	if(url != null){
-		window.open('https://twitter.com/share?url='+url+'&screen_name=enticconfio, '_blank');
+		window.open('https://twitter.com/share?url='+url+'&screen_name=enticconfio', '_blank');
 		//window.open('https://twitter.com/share?url=http://www.enticconfio.gov.co/&screen_name=enticconfio&text=Probando..', '_blank');
 	}
 }
